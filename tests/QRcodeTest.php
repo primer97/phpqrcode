7f8d77ca7961d914363e20983e47d839ec5e890f
primer97 <34078131+primer97@users.noreply.github.com>
2026-08-01 12:40:13 +0200

    Test encoded string by reading its output.

--- tests/QRcodeTest.php
+++ tests/QRcodeTest.php
@@ -12,6 +12,40 @@ class QRcodeTest extends TestCase
         $file=__DIR__."/out/qr1.png";
         QRcode::png("123456",$file,QRConstants::QR_ECLEVEL_L);
         $this->assertTrue(file_exists($file));
-        
     }
+    
+//region check QR content
+    public function testReadBasic()
+    {
+        $file=__DIR__."/out/qr2.png";
+        $in="123456";
+        QRcode::png($in,$file,QRConstants::QR_ECLEVEL_L);
+        $this->assertTrue(file_exists($file));
+        $reader = new \Zxing\QrReader($file);
+        $out=$reader->text();
+        $this->assertSame($in,$out);
+    }
+    
+    public function testEncodeAndReReadExtendChars()
+    {
+        $file=__DIR__."/out/qr3.png";
+        $in=" &aA|\\%/*-+.,^'#~?!<>[]`\n\r\"\t{}@;:§¤°é$";
+        QRcode::png($in,$file,QRConstants::QR_ECLEVEL_L);
+        $this->assertTrue(file_exists($file));
+        $reader = new \Zxing\QrReader($file);
+        $out=$reader->text();
+        $this->assertSame($in,$out);
+    }
+    
+    public function testEncodeAndReReadUtf8()
+    {
+        $file=__DIR__."/out/qr4.png";
+        $in="★→⚡✅";
+        QRcode::png($in,$file,QRConstants::QR_ECLEVEL_L);
+        $this->assertTrue(file_exists($file));
+        $reader = new \Zxing\QrReader($file);
+        $out=$reader->text();
+        $this->assertSame($in,$out);
+    }
+//endregion check QR content
 }
