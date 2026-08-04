d2f435f21c9bbce5f0ec838a1700367564915400
Fabien <hpfx@free.fr>
2026-08-04 10:09:02 +0200

    test : implement SentToBrowser test

--- tests/QRcodeTest.php
+++ tests/QRcodeTest.php
@@ -2,17 +2,41 @@
 
 namespace primer\phpqrcode;
 
+use phpmock\phpunit\PHPMock;
 use PHPUnit\Framework\TestCase;
 use Zxing\QrReader;
 
 class QRcodeTest extends TestCase
 {
+    use PHPMock;
     
     public function testPng()
     {
         $file=__DIR__."/out/qr1.png";
         QRcode::png("123456",$file,QRConstants::QR_ECLEVEL_L);
         $this->assertTrue(file_exists($file));
+    }
+    
+    public function testSentToBrowser()
+    {
+        // mock `header()` to prevent the "Cannot modify header information..." issue, and to check the right 'content-type' as well.
+        $header = $this->getFunctionMock(
+            'primer\\phpqrcode',
+            'header'
+        );
+        
+        $header->expects($this->once())
+               ->with('Content-type: image/png');
+        
+        ob_start();
+        QRcode::png("123456");
+     $out=ob_get_clean();
+     
+     // And now ensure we receive a png image:
+     $exp="\x89PNG\r\n\x1A\n"; // png image magic id
+     $img=substr($out, 0, 8);
+     $this->assertSame($exp,$img);
+     
     }
     
 //region check QR content
